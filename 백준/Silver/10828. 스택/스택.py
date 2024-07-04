6a30a4c029ac310f5d4811345355130a16023be3
Baek Min Hong <128719380+minhong620@users.noreply.github.com>
2024-07-04 18:12:40 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,25 @@
+import sys
+
+N = int(sys.stdin.readline())
+stack = []
+for _ in range(N):
+    command = sys.stdin.readline().split()
+    if command[0] == 'push':
+        stack.append(command[1])
+    elif command[0] == 'pop':
+        if len(stack) > 0:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        if len(stack) == 0:
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'top':
+        if len(stack) > 0:
+            print(stack[-1])
+        else:
+            print(-1)
