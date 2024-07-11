02a3217c4f3754377401c7032558481e74c614bc
Baek Min Hong <128719380+minhong620@users.noreply.github.com>
2024-07-11 17:43:54 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,15 @@
+# loop를 이용한 브투트포스(완전 탐색 문제)이다.
+
+N, M = map(int, input().split())
+card = list(map(int, input().split()))
+result = 0
+
+for i in range(N):
+    for j in range(i + 1, N):
+        for k in range(j + 1, N):
+            if card[i] + card[j] + card[k] > M:
+                continue
+            else:
+                result = max(result, card[i] + card[j] + card[k])
+
+print(result)
