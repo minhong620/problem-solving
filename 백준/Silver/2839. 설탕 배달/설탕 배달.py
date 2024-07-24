311cfcc11b3e71d2146bd52053bb474df3c93a49
Baek Min Hong <128719380+minhong620@users.noreply.github.com>
2024-07-24 23:21:17 +0900

    [Silver IV] Title: 설탕 배달, Time: 48 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,16 @@
+import sys
+import math
+
+N = int(sys.stdin.readline())
+bag = 0
+
+while N >= 0:
+    if N % 5 == 0:
+        bag += N // 5
+        print(bag)
+        break
+    N -= 3
+    bag += 1
+
+else:
+    print(-1)
