4eca1eb51b60000c5b16ec6fde95ceafce81b94d
Baek Min Hong <128719380+minhong620@users.noreply.github.com>
2024-07-27 23:53:34 +0900

    [Silver V] Title: 영화감독 숌, Time: 392 ms, Memory: 89540 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        int num = 666;
+        int count = 1;
+
+        while (count != N) {
+            num++;
+            if (String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
